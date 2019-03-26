d1aab58fffa941614484e59e9fd003cd129362b5
Robert Krahn <robert.krahn@gmail.com>
2019-03-26 04:57:26 -0700

    full system interface

--- l2l.js
+++ l2l.js
@@ -10,20 +10,24 @@ curl http://10.0.1.3:9011/lively-system-interface/dist/lively-system-interface-o
 
 */
 
+global.io = require('socket.io-client');
+global.babel = require('babel-standalone');
+require('systemjs');
+require('./lively.modules.js');
+require('./lively-system-interface.js');
+require('./lively.2lively_client.js');
 
-global.io = require("socket.io-client");
-global.babel = require("babel-standalone");
-require("systemjs");
-require("./lively.modules.js")
-require("./lively-system-interface-only-local.js")
-require("./lively.2lively_client.js");
-
-let url = `http://10.0.1.3:9011/lively-socket.io`;
+let url = `http://localhost:9011/lively-socket.io`;
 lively.l2l.client = lively.l2l.L2LClient.ensure({
   url,
-  namespace: "l2l",
-  info: {type: "l2l from node repl windows"}
+  namespace: 'l2l',
+  info: {type: 'l2l from node repl windows'},
 });
-lively.l2l.client.whenRegistered(20 * 1000)
-  .then(() => console.log("[l2l] online"))
-  .catch(err => console.error("[l2l] failed:", err));
+lively.l2l.client
+  .whenRegistered(20 * 1000)
+  .then(() => {
+    console.log('[l2l] online');
+  })
+  .catch(err => {
+    console.error('[l2l] failed:', err);
+  });
